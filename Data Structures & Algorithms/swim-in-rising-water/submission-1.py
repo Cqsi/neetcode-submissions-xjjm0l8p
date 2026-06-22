b15a214bde8b629d552bf3b9dfdf71f9ed41163d
Casimir Rönnlöf <casimirr04@gmail.com>
2026-06-22 19:10:12 +0300

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        
+        best = [[float("inf")] * len(grid) for _ in range(len(grid))]
+        best[0][0] = grid[0][0]
+        heap = []
+        heap.append((grid[0][0], 0, 0))
+
+        while heap:
+            cell, x, y = heapq.heappop(heap)
+            
+            if cell > best[x][y]:
+                continue
+
+            if x == len(grid)-1 and y == len(grid)-1:
+                return cell
+
+            directions = [[0,1], [0, -1], [1,0], [-1, 0]]
+            for direc in directions:
+                dx, dy = direc
+                if x+dx in range(len(grid)) and y+dy in range(len(grid)):
+                    newTime = max(cell, grid[x+dx][y+dy])
+                    if newTime < best[x+dx][y+dy]:
+                        best[x+dx][y+dy] = newTime
+                        heapq.heappush(heap, (newTime, x+dx, y+dy))
